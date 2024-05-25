84acfaa5fca9b1e5fb00dbb9bdd30c3752e007aa
SangeethaShanmugm <sanjuriya22@gmail.com>
2024-05-25 23:33:37 +0530

    updated

--- Module-4/Express_Router/app.js
+++ Module-4/Express_Router/app.js
@@ -3,7 +3,7 @@
 import express from "express"
 import { MongoClient } from "mongodb"
 import bodyParser from "body-parser"
-
+import { moviesRouter } from "./routes/movies.js"
 const app = express()
 const PORT = 5000
 
@@ -26,7 +26,7 @@ async function createConnection() {
     return client;
 }
 
-const client = await createConnection()
+export const client = await createConnection()
 
 // req => what we send to server
 // res => what we receive from server
@@ -34,20 +34,8 @@ app.get('/', function (req, res) {
     res.send('Hello Everyone')
 })
 
-//get all movies
-app.get('/getMovies', async function (req, res) {
-    const result = await client.db("node_may").collection("movies").find().toArray()//put document inside array
-    res.send(result)
-})
-
-
-//add new movies
-app.post('/addMovies', async function (req, res) {
-    const newMovies = req.body
-    console.log(newMovies)
-    const result = await client.db("node_may").collection("movies").insertOne(newMovies)
-    res.send(result)
-})
-
+app.use("/movies", moviesRouter)
 
 app.listen(PORT, () => console.log('The server started on the port', PORT))
+
+
